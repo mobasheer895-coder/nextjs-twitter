9e326f5713a9cf0c78ffe3d0d86c3331aa69e9a2
Mohammad <mobasheer895@gmail.com>
2026-08-30 00:39:32 +0300

    first commit

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,19 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ 
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma };
