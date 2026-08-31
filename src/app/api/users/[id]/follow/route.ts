5c79b922baa19059a7a4c91e5cc0c3bf581b5a3c
Mohammad <mobasheer895@gmail.com>
2026-08-31 12:49:47 +0300

    first commit

--- src/app/api/users/[id]/follow/route.ts
+++ src/app/api/users/[id]/follow/route.ts
@@ -8,100 +8,108 @@ interface props {
 
 export async function POST(request:NextRequest,{params}:props) {
     try {
+        // المستخدم Id
         const userId = parseInt((await params).id)
-
+        // نتأكد من ان لديه صلاحية (لديه توكن)
         const userPayload = await authenticateUser()
+        // اذا لا يوجد توكن يعيد خطأ
         if (!userPayload) {
             return NextResponse.json(
                 {message:'forbidden'},
                 {status:403}
             )
         }
-
+        // اذا كان المستخدم يتابع نفس حسابه يعيد خطأ
         if (userPayload.id == userId) {
             return NextResponse.json(
                 {message:'You cannot follow yourself'},
                 {status:400}
             )
         }
-
+        // التحقق من وجود المستخدم في قاعدة البيانات
         const followingUser = await prisma.user.findUnique({
             where:{id:userId}
         })
-
+        // اذا كان المستخدم غير موجود يعيد خطأ
         if (!followingUser) {
     return NextResponse.json(
         { message: 'User not found' },
         { status: 404 }
     )
 }
-
+    // الشخص الذي سيتابعه (Id) الشخص الذي يتابع مع (Id) يبحث في قاعدة البياات عن 
     const existingFollow = await prisma.followes.findFirst({
         where:{
             followerId:userPayload.id,
             followingId:userId
         }
     })
-
+    // اذا كان متابعه سابقا يعيد خطأ
     if (existingFollow) {
         return NextResponse.json(
             {message:'You are already following this user'},
             {status:400}
         )
     }
-
+    // انشائ المتابعة
     const newFollow = await prisma.followes.create({
         data:{
             followerId:userPayload.id,
             followingId:userId
         }
     })
-
+    // ارجاع رسالة بانه تمت المتابعة و قيمة المتابعة
     return NextResponse.json({message: 'User followed successfully', newFollow} , {status:201})
 
     } catch (error) {
         return NextResponse.json(
-            {message:'Internal server error'},
+            { 
+                message: 'internal server error', 
+                error: (error as Error).message
+            },
             {status:500}
         )
     }
 }
 export async function DELETE(request:NextRequest,{params}:props) {
     try {
-        
-    
+        // المستخدم Id
         const userId = parseInt((await params).id);
-
+        // نتأكد من ان لديه صلاحية (لديه توكن)
         const userPayload = await authenticateUser()
+        // اذا لا يوجد توكن يعيد خطأ
         if (!userPayload) {
             return NextResponse.json(
                 {message:'Unauthorized'},
                 {status:401}
             )
         }
-
+        // البحث عن سجل المتابعة الحالي المراد حذفه
         const existingFollow = await prisma.followes.findFirst({
             where: {
                 followerId: userPayload.id,
                 followingId: userId,
             },
         })
-
+        // اذا كان متابعه سابقا يعيد خطأ
         if (!existingFollow) {  
             return NextResponse.json(
                 { message: 'You are not following this user' },
                 { status: 404 }
             )
         }
-
+        // حذف المتابعة
         const deletefollow = await prisma.followes.delete({
             where:{id:existingFollow.id}
         })
-
+        // ارجاع رسالة بانه تم الغاء المتابعة و القيمة
         return NextResponse.json({message:'Unfollowed successfully',deletefollow} , {status:200})
     } catch (error) {
         return NextResponse.json(
-            {message:'internal server error'},
+            { 
+                message: 'internal server error', 
+                error: (error as Error).message
+            },
             {status:500}
         )
     }
--- src/app/api/users/[id]/posts/route.ts
+++ src/app/api/users/[id]/posts/route.ts
@@ -9,32 +9,34 @@ interface props {
 
 export async function GET(request:NextRequest,{ params }: props) {
     try {
+        // المستخدم Id
         const userId = parseInt((await params).id)
-
+        // نتأكد من ان لديه صلاحية (لديه توكن)
     const userPayload = await authenticateUser()
-
+        // اذا لا يوجد توكن يعيد خطأ
     if (!userPayload) {
         return NextResponse.json(
             {message:'forbidden'},
             {status:403}
         )
     }
-
+    // (Id) التحقق من وجود المستخدم ضمن قاعدة البيانات من خلال 
     const user = await prisma.user.findUnique({
         where:{id:userId}
     })
-
+    // اذا كان المستخدم غير موجود يعيد خطأ
     if (!user) {
             return NextResponse.json(
                 {message:'user not found'},
                 {status:404}
             )
         }
-
+        // المستخدم (Id) عرض البوستات حسب 
         const userPosts = await prisma.posts.findMany({
             where:{
                 userId:userId
             },
+            // يعرض معلومات المستخدم من جدول المستخدمين
             include:{
                 user:{
                     select:{
@@ -42,18 +44,22 @@ export async function GET(request:NextRequest,{ params }: props) {
                     }
                 }
             },
+            // يكون بترتيب من الاحدث الى الاقدم
             orderBy:{
                 createdAt:'desc'
             }
         })
-
+        // ارجاع قيمة بوستات المتسخدم
         return NextResponse.json(
             {userPosts},
             {status:200}
         )
     } catch (error) {
         return NextResponse.json(
-            {message:'internal server error'},
+            { 
+                message: 'internal server error', 
+                error: (error as Error).message
+            },
             {status:500}
         )
     }
